489c3c919f9998d84000fabe462a849fe58f7cde
DSD-18 <72644829+DSD-18@users.noreply.github.com>
2021-06-07 10:52:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,26 +12,6 @@ function setup(){
     video.hide();
 }
 
-function draw(){
-    image(video, 0, 0, 480, 380);
-    if(status != "")
-    {
-        objectDetector.detect(video,gotResult);
-        for (i = 0; i < objects.length; i++)
-        {
-            document.getElementById("status").innerHTML = "Status : Objects Detected"
-            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
-
-            fill("FF0000");
-            percent = floor(objects[1].confidence * 100);
-            text(objects[i].label + " " + percent + "&" , objects,{i}.x +15, objects[i].y + 15);
-            nofill();
-            stroke("FF0000");
-            rect(objects[i].x, objects[i].y, objects[i].width, objects,{i}.height);
-        }
-    }
-}
-
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
@@ -51,4 +31,24 @@ function gotResult(error, results){
     }
     console.log(results);
     objects = results;
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video,gotResult);
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Objects Detected"
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "&" , objects[i].x +15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
